46e1031d8daf97696eb019a86d648d85af7b548c
Jhona <83308085+aguick@users.noreply.github.com>
2021-06-05 23:06:08 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const client = new Discord.Client ()
+
+
+client.on("ready", () =>{
+    console.log("Estou pronta!")
+})
+
+client.on("message", (menssage) =>{
+    //console.log("Estou pronta!")
+})
+
+client.on("message", (menssage) =>{
+    //console.log("Estou pronta!")
+})
+
+
+client.login("DmbAlivg-JZiCYOP_B6mtWEb2iOLJ1SA")
+
+
+
